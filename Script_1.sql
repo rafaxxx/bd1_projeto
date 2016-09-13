f1a50d150ccdaf12e7ac0830bed10ed9b49231e9
Rafaxxx <rafaella.chaves@ccc.ufcg.edu.br>
2016-09-13 10:31:13 -0300

    modificando script1, adicionando entidades departamento, telefone, publicacao

--- Script_1.sql
+++ Script_1.sql
@@ -3,7 +3,7 @@ CREATE TABLE pessoa (
     nome                  VARCHAR(30) NOT NULL,
     dataNascimento        DATE NOT NULL,
     CONSTRAINT pessoa_pk  PRIMARY KEY (matricula)
-)
+);
 
 CREATE TABLE professor (
     matricula             INT,
@@ -12,10 +12,44 @@ CREATE TABLE professor (
     CONSTRAINT professor_pk PRIMARY KEY (matricula),
     CONSTRAINT professor_pessoa_fk FOREIGN KEY (matricula) REFERENCES pessoa (matricula),
     CONSTRAINT titulacao CHECK (UPPER(titulo) IN ('MESTRADO', 'DOUTORADO', 'POS-DOCTOR'))
-)
+);
 
 CREATE TABLE aluno (
     matricula             INT,
     CONSTRAINT aluno_pk PRIMARY KEY (matricula),
     CONSTRAINT aluno_pessoa_fk FOREIGN KEY (matricula) REFERENCES pessoa (matricula)    
-)
+);
+
+CREATE TABLE departamento (
+    codigo                INT NOT NULL,
+    nome                  VARCHAR(30) NOT NULL,
+    endereco              VARCHAR(70) NOT NULL,
+    coordenador           INT, 
+    telefones             INT,
+    CONSTRAINT departamento_pk PRIMARY KEY (codigo),
+    CONSTRAINT telefones_dpto_fk FOREIGN KEY (telefones) REFERENCES telefones (id)
+);
+
+CREATE TABLE telefones (
+    id                    INT NOT NULL AUTO_INCREMENT,
+    telefone              VARCHAR(15) NOT NULL,
+    CONSTRAINT telefones_pk PRIMARY KEY (id)
+    
+);
+
+CREATE TABLE publicacao (
+    codigo                INT NOT NULL,
+    titulo                VARCHAR(30) NOT NULL,
+    ano                   INT,
+    veiculo               VARCHAR(20) NOT NULL,
+    autor                 INT,
+    CONSTRAINT publicacao_pk PRIMARY KEY (codigo)
+);
+
+
+ALTER TABLE professor ADD CONSTRAINT professor_fk FOREIGN KEY (departamentoCod) REFERENCES departamento (codigo) deferrable;
+ALTER TABLE departamento ADD CONSTRAINT departamento_fk FOREIGN KEY (coordenador) REFERENCES professor (matricula) deferrable;
+ALTER TABLE pessoa ADD CONSTRAINT pessoa_fk FOREIGN KEY (publicacao) REFERENCES publicacao (codigo) deferrable;
+ALTER TABLE publicacao ADD CONSTRAINT publicacao_fk FOREIGN KEY (autor) REFERENCES pessoa (matricula) deferrable;
+
+
