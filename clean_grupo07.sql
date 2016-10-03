03800cfa3a475ff859b579883918e17352205497
Catarina <catarina.ufcg@gmail.com>
2016-10-03 03:45:17 -0300

    Script de remocao de dados

---
+++ clean_grupo07.sql
@@ -0,0 +1,46 @@
+BEGIN
+
+DELETE FROM graduacao;
+
+DELETE FROM pos;
+
+DELETE FROM aluno;
+
+DELETE FROM publicacao_pessoa;
+
+DELETE FROM publicacao;
+
+DELETE FROM patente;
+
+DELETE FROM professor_linhaPesquisa;
+
+DELETE FROM professor_projeto;
+
+DELETE FROM projeto_laboratorio;
+
+DELETE FROM projeto_linhaPesquisa;
+
+DELETE FROM linhaPesquisa;
+
+DELETE FROM departamento_telefone;
+
+DELETE FROM departamento;
+
+DELETE FROM agente_telefone;
+
+DELETE FROM projeto_agente;
+
+DELETE FROM agenteFinanciador;
+
+DELETE FROM recurso;
+
+DELETE FROM laboratorio;
+
+DELETE FROM projeto;
+
+DELETE FROM professor;
+
+DELETE FROM pessoa;
+
+END; 
+
